3f23b6d0cdeaa5b7938c89a7976980d0c513307d
Davi Matias <40278000+davimatiassg@users.noreply.github.com>
2023-05-04 18:15:29 -0300

    Update misc_functions.cpp

--- source/misc_functions.cpp
+++ source/misc_functions.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <map>
-#include <vector>
+#include <vector> 
+#include <fstream>
+
 using namespace std;
 //Função geral para printar informações e agenda guardadas em mapa.
 template<typename T1, typename T2>
@@ -10,4 +12,34 @@ void printMap(map<T1, T2> m)
     {
         cout<<(*it).first << ": " << (*it).second << endl;
     }
+}
+
+template<typename T1, typename T2>
+void saveMap(map<T1, T2> m, string FileName)
+{
+    ofstream file(FileName);
+    for(typename map<T1, T2>::iterator it = m.begin(); it != m.end(); it++)
+    {
+        file<<(*it).first << ";" << (*it).second << endl;
+    }
+    file.close();
+}
+
+template<typename T1, typename T2>
+map<T1, T2> recoverMap(map<T1, T2> m, string FileName)
+{
+    ifstream file(FileName);
+    map<T1, T2> * m = new map<T1, T2>(0);
+    while(true)
+    {
+        string field;
+        file>>field;
+        if(!file.eof()) break;
+        string l, r, del;
+        del = ";"
+        l = field.substr(0, field.find(del));
+        r = field.substr(field.find(del), field.size());
+        (*m).insert(make_pair(l, r));
+    }
+    return m;
 }
